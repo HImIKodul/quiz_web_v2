60b800a17be3aeccf067ea9727bd3bc7c11257a7
HImIKodul <148743900+HImIKodul@users.noreply.github.com>
2026-04-14 21:37:06 +0800

    Add ecosystem.config.js for PM2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps : [{
+    name: "quiz-web",
+    script: "node_modules/next/dist/bin/next",
+    args: "start",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: "production",
+    }
+  }]
+};
